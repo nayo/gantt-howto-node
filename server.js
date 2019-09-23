acc2e58a5874d3e5f6d867f92ad3a62be86f6707
Genaro Contreras Gutierrez <genaro_contreras@hotmail.com>
2019-09-23 12:00:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,27 @@ var db = mysql.createPool({
 	database: process.env.DB_NAME
 });
 
+db.query('CREATE TABLE IF NOT EXISTS `gantt_links` ( \
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+  `source` int(11) NOT NULL,\
+  `target` int(11) NOT NULL,\
+  `type` varchar(1) COLLATE utf8_unicode_ci NOT NULL\
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;', function (err, result, fields) {
+    if (err) throw new Error(err)
+})
+
+db.query('CREATE TABLE IF NOT EXISTS `gantt_tasks` ( \
+  `id` int(11) NOT NULL  AUTO_INCREMENT PRIMARY KEY, \
+  `text` varchar(255) COLLATE utf8_unicode_ci NOT NULL, \
+  `start_date` datetime NOT NULL, \
+  `duration` int(11) NOT NULL, \
+  `progress` float NOT NULL DEFAULT 0, \
+  `parent` int(11) NOT NULL, \
+  `sortorder` int(11) NOT NULL \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;', function (err, result, fields) {
+    if (err) throw new Error(err)
+})
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: true}));
 
